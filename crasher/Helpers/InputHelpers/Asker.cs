ae29f030ba1dce5f9368d9c066486d6bd5106173
Luca Di Bello <luca.dibello@samtrevano.ch>
2018-07-09 23:29:55 +0200

    Class that will manage the inputs at the start of the program

---
+++ crasher/Helpers/InputHelpers/Asker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+using crasher.Helpers.Validators;
+
+namespace crasher.Helpers.InputHelpers
+{
+    public class Asker
+    {
+        public Asker()
+        {
+            
+        }
+
+        public void Ask(String text, Validator validator)
+        {
+
+        }
+    }
+}
