fd220a164778e207324c2cca5d5bd094cb22696b
Luca Di Bello <luca.dibello@samtrevano.ch>
2018-07-09 23:28:36 +0200

    Enum that could be used for generating typical answers.

---
+++ crasher/Helpers/InputHelpers/AnswerType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crasher.Helpers.InputHelpers
+{
+    public enum AnswerType
+    {
+        YesOrNo = 0,
+    }
+}
